2bd33213d3b2b16833ba04736394ac5a475eefa5
blutheraptor <56351831+blutheraptor@users.noreply.github.com>
2019-10-15 17:05:28 +0530

    Create largestnumber.py

---
+++ largestnumber.py
@@ -0,0 +1,8 @@
+x=int(input("Enter the first number - "))
+y=int(input("Enter the Second number - "))
+if x>y:
+  print("The largest number is",x)
+elif x<y:
+  print("The largest number is",y)
+else:
+ print("Both the numbers are equal")
